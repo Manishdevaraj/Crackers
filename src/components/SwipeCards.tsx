02e1c2de882045c674713a7efbe18a9afe50b35b
manishdevaraj <manishdevaraj03@gmail.com>
2025-08-04 14:12:36 +0530

    final day ch4

--- src/components/SwipeCards.tsx
+++ src/components/SwipeCards.tsx
@@ -119,7 +119,7 @@ const SwipeCards = () => {
     {/* </div> */}
    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
   {cards.map((gift, index) => (
-    <div key={index} className="flex">
+    <div key={index} >
       <GiftCard product={gift} />
     </div>
   ))}
